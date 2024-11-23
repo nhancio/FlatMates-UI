c43bdeb7edc3340f2e8e4972760fb1a32b8d37e0
DidigamNithin <45207352+didigamnithin@users.noreply.github.com>
2024-11-24 01:07:11 +0530

    nithing changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flatemates_ui/res/font/font_size.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'navigation/app_routes/routes.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flatemates_ui/ui/screens/list_my_room_screen/list_my_room.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -21,8 +25,19 @@ class MyApp extends StatelessWidget {
           bodyLarge: TextStyle(fontSize: 16.0, color: Colors.black),
         ),
       ),
-      initialRoute: AppRoutes.bottomNavBar,
+      initialRoute: AppRoutes.welcome,
       getPages: AppRoutes.routes,
+      // initialRoute: AppRoutes.bottomNavBar,
+      // getPages: AppRoutes.routes,
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomeScreen(),
+//     );
+//   }
+// }
